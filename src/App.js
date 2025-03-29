11ec8741525fa30249bb4588a164fda5dbd55399
OlenaHryhlevych <hryhlevych.olena@gmail.com>
2025-03-29 21:17:24 +0200

    new commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
   return (
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <BrowserRouter basename="/web-prog">
       <Routes>
         <Route element={<Home />} />
         <Route path="/character/:id" element={<CharacterDetail />} />
