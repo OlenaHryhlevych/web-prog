957ffe2fd08cad7fb2a6ac671eeaf0c1d8136d02
OlenaHryhlevych <hryhlevych.olena@gmail.com>
2025-04-08 11:56:20 +0300

    hash router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from "react";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import CharacterDetail from "./components/CharacterDetail";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
   return (
-    <BrowserRouter basename="/web-prog">
+    <HashRouter>
       <Routes>
         <Route element={<Home />} />
         <Route path="/character/:id" element={<CharacterDetail />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
